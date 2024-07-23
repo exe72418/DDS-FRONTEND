f00822df22f1457d7de22a7a86f701d2dbcd114e
exe72418 <exequielzarate@gmail.com>
2024-07-23 20:51:02 -0300

    get all de clientes

--- fast/src/app/clientes/clientes.component.ts
+++ fast/src/app/clientes/clientes.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-clientes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
 export class ClientesComponent implements OnInit {
 
-  data: any[] = [];
   constructor(private apiService: ApiService) { }
+  clientes: Cliente[] = [];
 
   ngOnInit(): void {
     this.llenarData();
@@ -19,8 +20,26 @@ export class ClientesComponent implements OnInit {
 
   llenarData() {
     this.apiService.getData().subscribe(data => {
-      this.data = data;
+      this.clientes = data['data'].map((cliente: Cliente) => {
+        const clienteFormateado: Cliente = {
+          id: cliente.id,
+          apellidoNombre: cliente.apellidoNombre,
+          telefono: cliente.telefono,
+          email: cliente.email,
+          domicilio: cliente.domicilio,
+          cuit: cliente.cuit,
+        };
+        return clienteFormateado;
+      });
       console.log(data['data'][0]);
     });
   }
+}
+class Cliente {
+  apellidoNombre: string = "";
+  id: string = "";
+  telefono: string = "";
+  email: string = "";
+  domicilio: string = "";
+  cuit: string = "";
 }
