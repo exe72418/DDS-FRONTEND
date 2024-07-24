d48e34b00c619419b49e4eba00c3799b3926e384
exe72418 <exequielzarate@gmail.com>
2024-07-23 23:31:54 -0300

    Mejora visualmente a clientes

--- fast/src/app/app.routes.ts
+++ fast/src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
 import { ClientesComponent } from './clientes/clientes.component';
 
-export const routes: Routes = [{path: "", component: ClientesComponent}];
+export const routes: Routes = [{path: "clientes", component: ClientesComponent}];
