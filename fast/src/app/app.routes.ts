c2164ba747fa6b9c63629ba9aca8abcb8cacd62a
BrunoMar99 <brunoemartinez99@gmail.com>
2024-09-15 10:19:35 -0300

    REPARTIDOR TERMINADO

--- fast/src/app/app.routes.ts
+++ fast/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ClientesComponent } from './clientes/clientes.component';
 import { CrearClientesComponent } from './crear-clientes/crear-clientes.component';
 import { TipoproductoComponent } from './tipoproducto/tipoproducto.component';
 import { ProductosComponent } from './productos/productos.component';
+import { RepartidorComponent } from './repartidor/repartidor.component';
 
 export const routes: Routes = [
-  {path: "clientes", component: ClientesComponent},
-  {path: "crearclientes", component: CrearClientesComponent},
-  {path: "crearTipoProducto", component: TipoproductoComponent},
-  {path: "productos", component: ProductosComponent}
+  { path: "clientes", component: ClientesComponent },
+  { path: "crearclientes", component: CrearClientesComponent },
+  { path: "crearTipoProducto", component: TipoproductoComponent },
+  { path: "productos", component: ProductosComponent },
+  { path: "repartidores", component: RepartidorComponent }
 ];
