625825b66f6913228a47cc7611405e0b563493d1
BrunoMar99 <brunoemartinez99@gmail.com>
2025-03-29 16:20:59 -0300

    se agregaron los botones para reducir cantidad en el carrito

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -6,8 +6,6 @@ import { ProductosServiceService } from '../services/productos-service.service';
 import { Producto } from '../models/producto';
 import { Pedido } from '../models/pedido';
 import { LineaDeProducto } from '../models/lineaProducto';
-import { Route, Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-home-component',
@@ -16,29 +14,22 @@ import { Route, Router } from '@angular/router';
 })
 export class HomeComponentComponent implements OnInit, OnDestroy {
 
-
-  constructor(private _productoService: ProductosServiceService, private store: Store
-  ) {
-  }
-
+  constructor(private _productoService: ProductosServiceService, private store: Store) {}
 
   @Input() productos!: Producto[];
   pedido!: Pedido;
 
-
   ngOnInit(): void {
-
     this._productoService.getProductosActivos().subscribe((productos) => {
       this.productos = productos;
-    })
-    this.pedido = new Pedido()
-    this.pedido.lineas = []
+    });
+    this.pedido = new Pedido();
+    this.pedido.lineas = [];
     this.pedido.total = 0;
   }
 
   agregar(producto: Producto) {
     let lineaProd = new LineaDeProducto();
-
     lineaProd.cantidad = 1;
     lineaProd.producto = producto;
     lineaProd.subtotal = producto.precio;
@@ -50,37 +41,53 @@ export class HomeComponentComponent implements OnInit, OnDestroy {
         if (linea.producto.codigo == producto.codigo) {
           encontro = true;
           linea.cantidad++;
-          linea.subtotal = linea.subtotal + producto.precio;
-          this.pedido.total = this.pedido.total + linea.producto.precio;
+          linea.subtotal += producto.precio;
+          this.pedido.total += linea.producto.precio;
         }
-      })
+      });
       if (!encontro) {
         this.pedido.lineas = [...this.pedido.lineas, lineaProd];
-        this.pedido.total = this.pedido.total + lineaProd.producto.precio;
-
+        this.pedido.total += lineaProd.producto.precio;
       }
     } else {
       this.pedido.lineas = [...this.pedido.lineas, lineaProd];
-      this.pedido.total = this.pedido.total + lineaProd.producto.precio;
-
-
+      this.pedido.total += lineaProd.producto.precio;
     }
 
+    this.store.dispatch(new SetPedidosAction(_.cloneDeep(this.pedido)));
+  }
 
-    let pedidoState = this.pedido;
-    this.store.dispatch(new SetPedidosAction(_.cloneDeep(this.pedido)))
+  restar(producto: Producto) {
+    let index = this.pedido.lineas.findIndex(linea => linea.producto.codigo === producto.codigo);
 
+    if (index !== -1) {
+      let linea = this.pedido.lineas[index];
+
+      if (linea.cantidad > 1) {
+        linea.cantidad--;
+        linea.subtotal -= producto.precio;
+        this.pedido.total -= producto.precio;
+      } else {
+        this.eliminar(producto);
+      }
+
+      this.store.dispatch(new SetPedidosAction(_.cloneDeep(this.pedido)));
+    }
+  }
+
+  eliminar(producto: Producto) {
+    this.pedido.lineas = this.pedido.lineas.filter(linea => linea.producto.codigo !== producto.codigo);
+    this.pedido.total = this.pedido.lineas.reduce((total, linea) => total + linea.subtotal, 0);
+
+    this.store.dispatch(new SetPedidosAction(_.cloneDeep(this.pedido)));
   }
 
   consolelog() {
-    this.store.dispatch(new SetPedidosAction(_.cloneDeep(this.pedido)))
-
-    let pedido: Pedido = this.store.selectSnapshot(PedidoState.getPedido)
-
+    this.store.dispatch(new SetPedidosAction(_.cloneDeep(this.pedido)));
+    let pedido: Pedido = this.store.selectSnapshot(PedidoState.getPedido);
   }
 
   ngOnDestroy(): void {
-    this.store.dispatch(new SetPedidosAction(_.cloneDeep(this.pedido)))
-
+    this.store.dispatch(new SetPedidosAction(_.cloneDeep(this.pedido)));
   }
 }
