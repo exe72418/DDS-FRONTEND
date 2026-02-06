26ba59a2e1bd3d5eafc19e9a4cdadfe92b10ffd3
Alfre Reinoso <alfrereinoso96@gmail.com>
2026-02-06 13:43:51 -0300

    seteo el environment , acuerdanse de descomentar el localhost para probar local

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    serverUrl: 'http://localhost:3000/api/v2/'
+    //serverUrl: 'http://localhost:3000/api/v2/'
+    serverUrl: 'https://dds-backend-a.onrender.com'
 }
