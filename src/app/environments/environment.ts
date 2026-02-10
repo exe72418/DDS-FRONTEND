6fcb63047e20eb172f1c9b8b137abc31dca5956a
BrunoMar99 <brunoemartinez99@gmail.com>
2026-02-10 19:56:14 -0300

    solucionado de warnings

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    //serverUrl: 'http://localhost:3000/api/v2/'
-    serverUrl: 'https://dds-backend-a.onrender.com/api/v2/'
+    //serverUrl: 'http://localhost:3000/api/v2/' //--> dev
+    serverUrl: 'https://dds-backend-a.onrender.com/api/v2/' //--> prod
 }
